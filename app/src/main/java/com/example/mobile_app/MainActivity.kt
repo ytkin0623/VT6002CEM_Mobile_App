fc081b93d47e7bd82bb4ccebde77fbd47aac27b7
ytkin0623 <spmn999@gmail.com>
2024-01-15 15:07:13 +0800

    Initial commit: Login Page

---
+++ app/src/main/java/com/example/mobile_app/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.mobile_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executor
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        executor = ContextCompat.getMainExecutor(this)
+
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            })
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+        val biometricLoginButton =
+            findViewById<Button>(R.id.biometric_login)
+        biometricLoginButton.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val biometricStatusTextView =
+            findViewById<TextView>(R.id.biometric_status)
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                biometricStatusTextView.text = "App can authenticate using biometrics."
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                biometricStatusTextView.text = "No biometric features available on this device."
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                biometricStatusTextView.text = "Biometric features are currently unavailable."
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                // Prompts the user to create credentials that your app accepts.
+                biometricStatusTextView.text = "Biometric features are not enrolled."
+        }
+    }
+
+}
